bc2592a5ecd4ef9bad4f0dca8ca204b841d71f14
Mahdi Lotfian <80mahde@gmail.com>
2023-09-20 13:53:28 +0330

    some change in admin panel to make sure that only one person instance will create

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Person
 
-# Register your models here.
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    MAX_OBJECTS = 1
+
+    def has_add_permission(self, request):
+        if self.model.objects.count() >= self.MAX_OBJECTS:
+            return False
+        return super().has_add_permission(request)
