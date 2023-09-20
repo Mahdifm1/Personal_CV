cf96f1f4a5efb8211a90c09927f24736494d9f1d
Mahdi Lotfian <80mahde@gmail.com>
2023-09-20 13:50:49 +0330

    add person model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from solo.models import SingletonModel
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+
+class Person(models.Model):
+    # person name
+    name = models.CharField(max_length=50)
+    # 3 top skill separated by , like WEB DEVELOPER, GRAPHIC DESIGNER, PHOTOGRAPHER
+    short_skills = models.TextField(max_length=100)
+    # about me field
+    about = models.TextField()
+
+    age = models.PositiveSmallIntegerField(validators=[MinValueValidator(0)])
+    email = models.EmailField()
+    phone = models.CharField(max_length=13)
+    address = models.CharField(max_length=50)
+    languages = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='image')
+
