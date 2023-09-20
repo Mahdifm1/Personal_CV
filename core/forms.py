e1a0f71777ebebe33f85e5b0a62b10eb32ca8cb2
Mahdi Lotfian <80mahde@gmail.com>
2023-09-20 17:39:21 +0330

    update massage forms.py

--- core/forms.py
+++ core/forms.py
@@ -7,4 +7,5 @@ class ContactMeForm(forms.Form):
     subject = forms.CharField(max_length=100,
                               widget=forms.TextInput(attrs={'placeholder': 'Subject', 'class': 'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'E-mail', 'class': 'form-control'}))
-    massage = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your Message', 'class': 'form-control'}))
+    massage = forms.CharField(
+        widget=forms.Textarea(attrs={'placeholder': 'Your Message', 'class': 'form-control', 'cols': '', 'rows': ''}))
