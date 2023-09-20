9850596538446318de0d20a5bd056cfe1ebd2780
Mahdi Lotfian <80mahde@gmail.com>
2023-09-20 17:26:16 +0330

    update forms.py

--- core/forms.py
+++ core/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class ContactMeForm(forms.Form):
-    name = forms.CharField(max_length=60, widget=forms.TextInput(attrs={'placeholder': 'Name'}))
-    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': 'Subject'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'E-mail'}))
-    massage = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your Message'}))
+    name = forms.CharField(max_length=60,
+                           widget=forms.TextInput(attrs={'placeholder': 'Name', 'class': 'form-control'}))
+    subject = forms.CharField(max_length=100,
+                              widget=forms.TextInput(attrs={'placeholder': 'Subject', 'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'E-mail', 'class': 'form-control'}))
+    massage = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Your Message', 'class': 'form-control'}))
